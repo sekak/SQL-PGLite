0573664366637843c59b6875b4a73e7d098dcf7f
sekak <ahmadesekak@gmail.com>
2025-09-27 12:36:11 +0100

    FEAT9: Challenge

--- query.sql
+++ query.sql
@@ -1,11 +1,35 @@
 /*
-	Select the brand, model, condition and year from cars
-		Where the year is 1961, 1963, 1965, 1967 or 1969
-		and the condition is 3 or higher
-		and sold is false
+	Select brand, model, and color from cars
+		where the color is 'red'
+		and the brand is not 'Ferrari'
+		and the car has not been sold
 */
 
-SELECT brand, model, condition, sold, year FROM cars
-	WHERE year IN (1961, 1963, 1965, 1967, 1969)
-    AND condition >= 3
-    AND sold = false;
+-- SELECT brand, model, color FROM cars
+--   WHERE color = 'red'
+--     AND brand != 'Ferrari'
+--     AND sold = false;
+
+
+/*Select brand, model, and color from cars
+	where the color is not red, blue, or white
+	and the brand is none of: Aston Martin, Bentley or Jaguar
+	and sold is false  
+*/
+
+-- SELECT brand, model, color FROM cars 
+--   WHERE color NOT IN ('red', 'blue', 'white')
+--   AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
+--   AND  sold = false;
+
+/*
+	Select brand, model, year, sold from cars
+		where the brand is 'Dodge' and year is in the 60s
+		or the brand is either 'Ford' or 'Triumph' and the car is from the 70s
+		only select cars where sold is not true
+*/
+
+-- SELECT brand, model, year, sold FROM cars
+--   WHERE ((brand = 'Dodge' AND year BETWEEN 1960 AND 1969)
+--     OR (brand IN ('Ford', 'Triumph') AND year BETWEEN 1970 AND 1979))
+-- 	AND sold IS NOT true;
