fbdfc615d6296ffcb3906bc76239cf45bf209921
sekak <ahmadesekak@gmail.com>
2025-09-27 12:49:41 +0100

    Feat#11: ORDER BY - WHERE

--- query.sql
+++ query.sql
@@ -1,8 +1,12 @@
 /*
-	Select the brand, model, condition and price from cars
+		Select the brand, model, condition and price from cars
+		where the car is not sold
+		and the condition is not 5
 		order the table by condition in descending order
 		and by price in ascending order
 */
 
 SELECT brand, model, condition, price FROM cars
-  ORDER BY condition DESC, price ASC;
+  WHERE sold = false
+	AND condition  != 5
+	ORDER BY condition DESC, price ASC;
