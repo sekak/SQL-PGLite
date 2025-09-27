efab33ca2257b04c3a0081b6c361b42a664d41df
sekak <ahmadesekak@gmail.com>
2025-09-27 12:46:25 +0100

    Feat#10: ORDER BY

--- query.sql
+++ query.sql
@@ -1,35 +1,8 @@
 /*
-	Select brand, model, and color from cars
-		where the color is 'red'
-		and the brand is not 'Ferrari'
-		and the car has not been sold
+	Select the brand, model, condition and price from cars
+		order the table by condition in descending order
+		and by price in ascending order
 */
 
--- SELECT brand, model, color FROM cars
---   WHERE color = 'red'
---     AND brand != 'Ferrari'
---     AND sold = false;
-
-
-/*Select brand, model, and color from cars
-	where the color is not red, blue, or white
-	and the brand is none of: Aston Martin, Bentley or Jaguar
-	and sold is false  
-*/
-
--- SELECT brand, model, color FROM cars 
---   WHERE color NOT IN ('red', 'blue', 'white')
---   AND brand NOT IN ('Aston Martin', 'Bentley', 'Jaguar')
---   AND  sold = false;
-
-/*
-	Select brand, model, year, sold from cars
-		where the brand is 'Dodge' and year is in the 60s
-		or the brand is either 'Ford' or 'Triumph' and the car is from the 70s
-		only select cars where sold is not true
-*/
-
--- SELECT brand, model, year, sold FROM cars
---   WHERE ((brand = 'Dodge' AND year BETWEEN 1960 AND 1969)
---     OR (brand IN ('Ford', 'Triumph') AND year BETWEEN 1970 AND 1979))
--- 	AND sold IS NOT true;
+SELECT brand, model, condition, price FROM cars
+  ORDER BY condition DESC, price ASC;
